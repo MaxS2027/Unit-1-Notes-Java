bb7b33c4bead9c071937847644ef4b3bfe75b698
MaxS2027 <max.schwartz2027@gbwl.org>
2025-09-12 12:56:06 +0000

    1.2 notes

--- Main.java
+++ Main.java
@@ -1,6 +1,20 @@
+// every java file is a class
 public class Main {
-
+// main method is the entryway to the program    
    public static void main(String []args) {
       System.out.println("Hello");
+      System.out.println("Hi");
+      int score;
+      // declare a new variable
+      score = 72;
+      // INITIALIZE combines declaring + assigning
+   String pokemon = "Pikachu";
+      double healthPercent = 0.75;
+      boolean isAlive = true;
+
+      // use the plus sign to Concatentate (combine)strings
+      String greeting = "Hello" + "world";
+      System.out.println(greeting);
+      // quotation marks contain STRING LITERALS
    }
 }
