b8e49a672fd76eda6ec8f7e7321ed3b0fa0ab63c
MaxS2027 <max.schwartz2027@gbwl.org>
2025-09-15 13:38:43 +0000

    1.3 notes

--- Main.java
+++ Main.java
@@ -16,5 +16,32 @@ public class Main {
       String greeting = "Hello" + "world";
       System.out.println(greeting);
       // quotation marks contain STRING LITERALS
+         //output difference between .print and .println
+         System.out.print ("Hi");
+         System.out.println("There");
+         System.out.print ("!");
+      // ESCAPE: use \ to include special characters
+      String message = "I woke up and said, \"HELLO WORLD\"";
+      System.out.println(message);
+      System.out.println(4 + 3);
+      System.out.println(4 - 1);
+      System.out.println(6 * 7);
+      System.out.println(5 / 3); //truncation
+      // integer division results in TRUNCATIOn which is basicly throwing away decimal places
+      System.out.println(5.0/3); //double
+      
+      
+      //COMPOUND expressions follow PEMDAS
+      double result = 2 + 3 * 2;
+      System.out.println(result);
+      int remain = 23 % 2
+      System.out.println(remain);
+      System.out.println( 628%2);
+      // watch out for plus sign
+      System.out.println(3+"3");
+  
    }
+
+   
 }
+
